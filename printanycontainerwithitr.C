6e7c67b84b94d79a3d7bfdd8988820c40ef9709e
teager2 <joo.kang.hyun2@gmail.com>
2023-07-03 16:07:29 +0900

    printanycontainerwithitr, with some comments

--- printanycontainerwithitr.C
+++ printanycontainerwithitr.C
@@ -2,28 +2,6 @@
 #include <vector>
 #include <list>
 
-template <typename Container>
-void print(const Container & c, std::ostream & out = std::cout)
-{
-    if (c.empty())
-        out << "(empty)"
-    else
-    {
-        auto itr = begin(c);
-        Container::const_iterator itr = begin(c);
-
-        out << "size : " << c.size() << endl;
-        out << "size(itr ver1.) : " << (*itr).size() << endl;
-        out << "size(itr ver2.) : " << irt.size() << endl;
-
-        out << "print items" << endl;
-        out << "[ " << *itr++; // first item
-
-        while(itr != end(c))
-    }       out << ", " << *itr++;
-        out << " ]" << endl;
-}
-
 template<typename Container>
 auto begin(Container & c) -> decltype(c.begin())
 {
@@ -48,12 +26,37 @@ auto end(const Container & c) -> decltype(c.end())
     return c.end();
 }
 
+template <typename Container>
+void print(const Container & c, std::ostream & out = std::cout)
+{
+    if (c.empty())
+        out << "(empty)";
+    else
+    {
+        // auto itr = begin(c);
+        auto itr = c.begin();
+        // Container::const_iterator itr = begin(c);
+
+        out << "size : " << c.size() << std::endl;
+        // out << "size(itr ver1.) : " << (*itr).size() << std::endl;
+        // out << "size(itr ver2.) : " << itr->size() <<std::endl;
+
+        out << "print items" << std::endl;
+        out << "[ " << *itr++; // first item
+
+        while(itr != end(c))
+            out << ", " << *itr++;
+        out << " ]" << std::endl;
+    }
+}
+
+
 int main()
 {
     std::vector<int> v {6,5,1,4,2};
     std::list<int> l {6,5,1,4,2};
 
-    
+    print(v,std::cout);
 
     return 0;
 }
