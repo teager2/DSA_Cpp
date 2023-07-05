8c31b7ca29b23b83d849b9cdb82bdd0c79c03fd8
teager2 <joo.kang.hyun2@gmail.com>
2023-07-05 20:12:06 +0900

    error check version - refer comments

--- List.C
+++ List.C
@@ -2,6 +2,46 @@
 // Refer "Data Structures and algorithm analysis in C++" by Weiss, 3.5, page 91~
 // 2023-07-05
 
+// Error check version
+
+// Major error hosts
+// 1. Uninitiallized List           
+    // --> the List's pointer is nullptr
+    // --> not allowed in all cases. (the erase/insert/++/* should not be applied)
+
+// 2. Uninitiallized (const_)iterator
+    // --> the current's pointer is nullptr
+    // --> not allowed in all cases. (the erase/insert/++/* should not be applied)
+
+// 3. Iterator at endmarker (at times)
+    // --> the current's next pointer is nullptr
+    // --> erase/++/* should not be applied, the only insertion is allowed (which is pop_back())
+    // --> should be checked at the List's member function insert/erase
+    // --> should be checked at the iterator's operator ++/* 
+
+// 4. Iterator at head
+    // --> the current is in head. somthing is wrong from the beginning.
+    // --> not allowed in all cases. (the erase/insert/++/* should not be applied)
+    // --> c.f.) operator ++ might be made to be valid in some cases, but not valid here
+
+// 5. Iterator for different List 
+    // --> the iterator is from other Container
+    // --> not allowed in erase/insert 
+    // --> the iterator now have the List from which it was constructed as a member variable.
+    // --> c.f.) ++/* are not List's member function. they're iterator's operators
+
+// assertions and exceptions
+// For major error hosts 1,2,4, they are intrinsically invalid.
+    // --> they are together called as assertIsValid and check for erase/insert/++/*.
+    // --> throw IteratorOutOfBoundsException 
+// For major error host 3, the endmarker check would be performed for erase/++/* in addition to the assertIsValid test
+    // --> throw IteratorOutOfBoundsException ("iterator is at endmarker, erase/++/* is not possible")
+// For major error host 5, the iterator's List check would be performed when eras/insert is performed.
+    // --> throw IteratorMisMatchException
+
+// Haven't studied exception handling in c++...
+// So the exceptions will be just warning phrases for now.
+
 #include <iostream>
 
 template <typename Object>
@@ -26,7 +66,7 @@ class List
         class const_iterator
         {
             public:
-                const_iterator() : current{nullptr} {} // constructor (if not propely initialized, this iterator refers nullptr)
+                const_iterator() : theList{nullptr},current{nullptr} {} // constructor (if not propely initialized, this iterator refers nullptr)
 
                 const Object & operator* () const
                 {
@@ -35,12 +75,24 @@ class List
 
                 const_iterator & operator++() //prefix operator
                 {
+                    assertIsValid();
+                    if(current->next==nullptr)
+                    {
+                        std::cout << "Cannot retrieve from the endmarker."<< std::endl;
+                        exit(1);
+                    }
                     current = current->next;
                     return *this;
                 }
 
                 const_iterator operator++(int) //postfix operator / return const_iterator itself (slower)
                 {
+                    assertIsValid();
+                    if(current->next==nullptr)
+                    {
+                        std::cout << "Cannot retrieve from the endmarker."<< std::endl;
+                        exit(1);
+                    }
                     const_iterator old = *this;
                     ++(*this); //using the prefix;
                     return old; // return old itself is the key of postfix operator
@@ -57,14 +109,32 @@ class List
                 }
 
             protected:
+                const List<Object> *theList; // the List from which this const_iteraotr was constructed
                 Node *current;
+
                 Object & retrieve() const
                 {
+                    assertIsValid();
+                    if(current->next==nullptr)
+                    {
+                        std::cout << "Cannot retrieve from the endmarker."<< std::endl;
+                        exit(1);
+                    }
                     return current->data;
                 }
-                const_iterator(Node *p) : current{p} {}
+                const_iterator(const List<Object> & lst, Node *p) : theList{&lst}, current{p} {}
 
+                void assertIsValid() const
+                {
+                    if(theList == nullptr || current == nullptr || current == theList -> head)
+                    {
+                        std::cout << "Iterator or List not Valid" << std::endl;
+                        exit(1);
+                        // throw IteratorOutofBoundsException{};
+                    }
+                }
                 friend class List<Object>;
+                
         };
 
         class iterator : public const_iterator //base class is const_iterator
@@ -82,23 +152,67 @@ class List
                     return const_iterator::operator*(); // repeat again so the ambiguity is avoided.
                 }
 
+
                 //due to changed return type (const_iterator -> iterator)
                 //we provide a new pair of operator++
                 iterator & operator++() //prefix
                 {
+                    assertIsValid();
+                    if(this->current->next==nullptr)
+                    {
+                        std::cout << "Cannot retrieve from the endmarker."<< std::endl;
+                        exit(1);
+                    }
                     this->current = this->current->next;
                     return *this;
                 }
 
                 iterator operator++(int) //no & here
                 {
+                    assertIsValid();
+                    if(this->current->next==nullptr)
+                    {
+                        std::cout << "Cannot retrieve from the endmarker."<< std::endl;
+                        exit(1);
+                    }
                     iterator old = *this;
                     ++(*this);
                     return old;
                 }
 
             protected:
-                iterator(Node *p) : const_iterator{p} {}//constructor 
+                // iterator(const List<Object> & lst, Node *p) : const_iterator{&lst,p} {}//constructor 
+                iterator(const List<Object> & lst, Node *p) //constructor 
+                {
+                    this->theList = &lst;
+                    this->current = p;
+                }
+
+                void assertIsValid() const // inherited from const_iterator, but i should makr it const? why?
+                {
+                    if(this->theList == nullptr)
+                    {
+                        std::cout << "iterator1";
+                        std::cout << "Iterator or List not Valid" << std::endl;
+                        exit(1);
+                        // throw IteratorOutofBoundsException{};
+                    }
+                    if(this->current == nullptr )
+                    {
+                        std::cout << "iterator2";
+                        std::cout << "Iterator or List not Valid" << std::endl;
+                        exit(1);
+                        // throw IteratorOutofBoundsException{};
+                    }
+                    if(this->current == this->theList->head)
+                    {
+                        std::cout << "iterator3";
+                        std::cout << "Iterator or List not Valid" << std::endl;
+                        exit(1);
+                        // throw IteratorOutofBoundsException{};
+                    }
+
+                }
 
                 friend class List<Object>;
         };
@@ -154,19 +268,29 @@ class List
         iterator begin() //construct the iterator whose member(Node*) points the first item
         {
             // return {head->next};
-            return iterator{head->next};
+            // return iterator{*this,head->next};
+
+            iterator itr{*this, head->next};
+            return itr;
         }
         iterator end()
         {
-            return iterator{tail};
+            // return iterator{tail};
+
+            return iterator{*this,tail};
         }
         const_iterator begin() const
         {
-            return const_iterator{head->next};
+            // return const_iterator{head->next};
+
+            const_iterator itr{*this, head->next};
+            return itr;
         }
         const_iterator end() const
         {
-            return const_iterator{tail};
+            // return const_iterator{tail};
+
+            return const_iterator{*this, tail};
         }
 
         int size() const
@@ -232,6 +356,14 @@ class List
 
         iterator insert( iterator itr, const Object & x)
         {
+            itr.assertIsValid();
+            if(itr.theList != this)
+            {
+                std::cout << "the iterator is not from the List from which it was constructed" << std::endl;
+                exit(1);
+            }
+            
+                
             Node *p = itr.current;
             theSize++;
             // Node * newNode = new Node{x,p->prev,p};
@@ -243,16 +375,35 @@ class List
 
         iterator insert( iterator itr, Object && x)
         {
+            itr.assertIsValid();
+            if(itr.theList != this)
+            {
+                std::cout << "the iterator is not from the List from which it was constructed" << std::endl;
+                exit(1);
+            }
             Node *p = itr.current;
             theSize++;
-            return {p->prev=p->prev->next=new Node{std::move(x), p->prev, p}}; //constructor
-            // return iterator{p->prev=p->prev->next=new Node{std::move(x), p->prev, p}}; //constructor
+            // return {p->prev=p->prev->next=new Node{std::move(x), p->prev, p}}; //constructor
+
+            return iterator{*this,p->prev=p->prev->next=new Node{std::move(x), p->prev, p}}; //constructor
         }
 
         iterator erase(iterator itr) //erase single node
         {
+            itr.assertIsValid();
+            if(itr.current->next==nullptr)
+            {
+                std::cout << "Cannot retrieve from the endmarker."<< std::endl;
+                exit(1);
+            }
+            if(itr.theList != this)
+            {
+                std::cout << "the iterator is not from the List from which it was constructed" << std::endl;
+                exit(1);
+            }
             Node *p = itr.current;
-            iterator retVal{p->next}; // now the p->next refers the current node
+            // iterator retVal{p->next}; // now the p->next refers the current node
+            iterator retVal{*this,p->next}; // now the p->next refers the current node
             p->prev->next = p->next;
             p->next->prev = p->prev;
             delete p; // reclaim the erased item;
@@ -318,15 +469,7 @@ int main()
     List<int> l1;
     l1.push_back(1);
     l1.push_back(2);
-
-    print(l1,std::cout);
-
-    l1.clear();
-    l1.push_back(5);
-    l1.push_back(6);
-
-    print(l1,std::cout);
-    l1.pop_front();
+    l1.push_back(3);
     print(l1,std::cout);
 
     return 0;
